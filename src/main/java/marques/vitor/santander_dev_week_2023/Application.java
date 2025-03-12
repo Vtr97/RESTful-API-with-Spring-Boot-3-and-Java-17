12d734afd41c412ef76471858c0fcdb2e014206d
Vitor Marques <vitormr97@live.com>
2025-03-12 18:19:37 -0300

    first commit

---
+++ src/main/java/marques/vitor/santander_dev_week_2023/Application.java
@@ -0,0 +1,13 @@
+package marques.vitor.santander_dev_week_2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}

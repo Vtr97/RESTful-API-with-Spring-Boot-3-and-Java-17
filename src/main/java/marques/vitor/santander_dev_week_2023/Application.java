3fc73cec324cb14be57d0ef97a919cdd34bbcd87
Vitor Marques <vitormr97@live.com>
2025-03-13 21:27:57 -0300

    fix: fixed cores

--- src/main/java/marques/vitor/santander_dev_week_2023/Application.java
+++ src/main/java/marques/vitor/santander_dev_week_2023/Application.java
@@ -3,6 +3,10 @@ package marques.vitor.santander_dev_week_2023;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default server")})
 @SpringBootApplication
 public class Application {
 
